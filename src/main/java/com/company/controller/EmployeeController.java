919f733013d6d78215e00f9007761223725841de
Ümüt Günaydın <umutgunaydin65@gmail.com>
2023-03-28 14:24:34 +0300

    employee and stateList models are added to form

--- src/main/java/com/company/controller/EmployeeController.java
+++ src/main/java/com/company/controller/EmployeeController.java
@@ -1,6 +1,9 @@
 package com.company.controller;
 
+import com.company.bootstrap.DataGenerator;
+import com.company.model.Employee;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/employee")
 public class EmployeeController {
     @GetMapping("/register")
-    public String createEmployee() {
+    public String createEmployee(Model model) {
 
+        model.addAttribute("employee",new Employee());
+        model.addAttribute("stateList", DataGenerator.getAllStates());
 
         return "employee/employee-create";
     }
